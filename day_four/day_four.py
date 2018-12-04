a30860dc1d95dcc5c0f10c014e54e7e31848120f
StephenClarkApps <contact.stephenclark@gmail.com>
2018-12-04 18:59:29 +0000

    ...

--- day_four/day_four.py
+++ day_four/day_four.py
@@ -17,34 +17,38 @@ class Record:
         self.falls_asleep = falls_asleep
         self.wakes = wakes
 
+
+def ParseLine(line):
+        words = line.split()
+        date = words[0][1:] + " " + words[1][:-1]
+        parsed_date = parse(date)
+        year = parsed_date.year
+        month = parsed_date.month
+        day = parsed_date.day
+        hour = parsed_date.hour
+        mins = parsed_date.minute
+
+        #print (year, month, day, hour, mins)
+        shift_start = False
+        guard_id = ""
+        falls_asleep = False
+        wakes = False
+
+        if (words[2] == "Guard"):
+            shift_start = True
+            guard_id = words[3]
+        elif (words[2] == "wakes"):
+            wakes = True
+        elif (words[2] == "falls"):
+            falls_asleep = True
+        return Record(year, month, day, hour, mins, shift_start, guard_id, falls_asleep, wakes)
+
 # maybe a data structure with the line, the day, month, year, the time,
 # if its a Fall/Wake or on-shift message
 
 for line in open('input.txt'):
-    words = line.split()
-    
-    date = words[0][1:] + " " + words[1][:-1]
-    parsed_date = parse(date)
-    year = parsed_date.year
-    month = parsed_date.month
-    day = parsed_date.day
-    hour = parsed_date.hour
-    mins = parsed_date.minute
-
-    #print (year, month, day, hour, mins)
-    shift_start = False
-    guard_id = ""
-    falls_asleep = False
-    wakes = False
-
-    if (words[2] == "Guard"):
-        shift_start = True
-        guard_id = words[3]
-        print (guard_id)
-    elif (words[2] == "wakes"):
-        wakes = True
-    elif (words[2] == "falls"):
-        falls_asleep = True
+    record = (ParseLine(line))
+    print record.year
 
 # need to read in the file
 # parse it and then process
