a5090aaec180b3fe6c3903f1d6da6b58c8e2e815
StephenClarkApps <contact.stephenclark@gmail.com>
2018-12-04 20:49:13 +0000

    HMM

--- day_four/day_four.py
+++ day_four/day_four.py
@@ -6,6 +6,11 @@ from dateutil.parser import parse
 import numpy as np
 
 records = []
+guard_ids_by_mins_asleep = {}
+
+# Somehow have to track number of occurances of sleeping in a given minute
+# indexed by guard ??
+# {Guard : (min, count)}
 
 class Record:
     def __init__(self, date, year, month, day, hour, mins, shift_start, guard_id, falls_asleep, wakes):
@@ -33,17 +38,18 @@ def ParseLine(line):
 
         #print (year, month, day, hour, mins)
         shift_start = False
-        guard_id = ""
+        guard_id = 0
         falls_asleep = False
         wakes = False
-
         if (words[2] == "Guard"):
             shift_start = True
-            guard_id = words[3]
+            guard_id = int(words[3][1:])
         elif (words[2] == "wakes"):
             wakes = True
+            guard_id = 0
         elif (words[2] == "falls"):
             falls_asleep = True
+            guard_id = 0
         return Record(date, year, month, day, hour, mins, shift_start, guard_id, falls_asleep, wakes)
 
 # Read in data from the input file and append records to an array of records
@@ -59,13 +65,32 @@ current_guard_id = 0
 last_fall_asleep_minute = 0
 last_wake_up_minute = 0
 for record in records:
-    current_guard_id = record.guard_id
+    #current_guard_id = record.guard_id
     if record.shift_start == True:
-        print (current_guard_id + " Has started a shift. At: " + str(record.date))
+        current_guard_id = record.guard_id
+        #print (str(current_guard_id) + " Has started a shift. At: " + str(record.date))
+        #print (current_guard_id)
     if record.falls_asleep:
-        print ("He/she has fallen asleep @"  + str(record.date))
+        #print ("He/she has fallen asleep @"  + str(record.date))
         last_fall_asleep_minute = int(record.mins)
     if record.wakes:
-        print ("He/she has woken again @"  + str(record.date))
+        #print ("He/she has woken again @"  + str(record.date))
         last_wake_up_minute = int(record.mins)
-        print("They've been asleep for: " + str(last_wake_up_minute - last_fall_asleep_minute) + "mins")
+        key = current_guard_id
+        guard_ids_by_mins_asleep[key] = guard_ids_by_mins_asleep.get(key , 0)  + (last_wake_up_minute - last_fall_asleep_minute)
+        #print("They've been asleep for: " + str(last_wake_up_minute - last_fall_asleep_minute) + "mins")
+
+previous_max_mins = 0
+linked_guard = 0
+for key, value in guard_ids_by_mins_asleep.items():
+    print ("Guard: ")
+    print(key)
+    print ("Mins: ")
+    print(value)
+    if value > previous_max_mins:
+        previous_max_mins = value
+        linked_guard = key
+print("Linked Guard: ")
+print (linked_guard)
+print ("AT")
+print (previous_max_mins)
