cbfbb9d856b913724c11622b9561784c36c6bbf1
StephenClarkApps <contact.stephenclark@gmail.com>
2018-12-04 02:14:16 +0000

    Simplify solution to three, use defaultdict from collections, solve part 1

--- day_three/three.py
+++ day_three/three.py
@@ -1,6 +1,7 @@
 import re
+from collections import defaultdict
 
-grid = {}
+grid = defaultdict(int)
 
 class Predition:
     def __init__(self, id, left, top, height, width):
@@ -10,47 +11,30 @@ class Predition:
         self.height = height
         self.width = width
 
-
-line = ("#1 @ 912,277: 27x20")
-
+#line = ("#1 @ 912,277: 27x20")
 
 def GetPrediction(some_string):
     extracted = re.findall(r"\w+",some_string)
-    id = extracted[0]
-    left = extracted[1]
-    top = extracted[2]
+    id = int(extracted[0])
+    left = int(extracted[1])
+    top = int(extracted[2])
     height_and_width = re.split(r'(\d+)', extracted[3])
-    height = height_and_width[1]
-    width = height_and_width[3]
+    height = int(height_and_width[1])
+    width = int(height_and_width[3])
     return Predition(id, left, top, height, width)
 
-def CreateSetOfCoordinatesFromPrediction(prediction):
-    theXCoordinates = []
-    theYCoordinates = []
-    xcounter = 0
-    ycounter = 0
 
-    width_int = int(prediction.width)
-    height_int = int(prediction.height)
+input = open('input.txt').readlines()
 
-    for x in range(width_int):
-        theXCoordinates.append(int(prediction.left) + xcounter)
-        xcounter += 1
-    for y in range(height_int):
-        theYCoordinates.append(int(prediction.top) + ycounter)
-        ycounter += 1
+for row in input:
+    pred = GetPrediction(row)
+    for dx in range(pred.width):
+        for dy in range(pred.height):
+            grid[(pred.left + dy, pred.top + dx)] +=1
 
-    #acount = 0
-    #bcount = 0
-    #for a in range(len(theXCoordinates)):
-        #acount += 1
-        #for b in range (len(theYCoordinates)):
-            #grid[a] = b
-            #bcount += 1
+overlap_total = 0
+for (r,c),value in grid.items():
+    if value >= 2:
+        overlap_total += 1
 
-    print (str(theXCoordinates))
-    print (str(theYCoordinates))
-    #print (grid)
-
-pred = GetPrediction(line)
-CreateSetOfCoordinatesFromPrediction(pred)
+print (overlap_total)
