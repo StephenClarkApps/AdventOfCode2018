c4a8fc5b7925533c6ac3f4bb667d4f5cf9834058
StephenClarkApps <contact.stephenclark@gmail.com>
2018-12-05 11:15:09 +0000

    ...

--- day_five/five.py
+++ day_five/five.py
@@ -1,6 +1,5 @@
 import re
 input_file = open('input.txt').read()
-#input_file = "dabAcCaCBAcCcaDA"
 input = list(input_file)
 
 def opposites(x, y):
@@ -16,14 +15,7 @@ after = ['Q']
 def OnePass():
     counter = 0
     for x in input:
-        if counter == 0:
-            print ("S")
-            # if input[counter].lower() == input[counter + 1].lower():
-            # #check if have differnt case to each other
-            #     if opposites(input[counter], input[counter + 1]) == True:
-            #         del input[counter]
-            #         del input[counter + 1]
-        else:
+        if counter !=0:
             if input[counter - 1].lower() == input[counter].lower():
                 #check if have differnt case to each other
                 if opposites(input[counter - 1], input[counter]) == True:
@@ -31,21 +23,14 @@ def OnePass():
                     del input[counter - 1]
         counter += 1
 
-print (len(input))
     #Check that two Chracters are the same, but have different cases
 while before != after:
-    print ("A")
     before = input.copy()
     OnePass()
     after = input.copy()
-    if before == after:
-        print ("Before == After ??")
-
-print (''.join(input))
 
 regex = re.compile('[^a-zA-Z]')
 this = ''.join(input)
 that = regex.sub('', this)
 
-print (input)
 print (len(that))
