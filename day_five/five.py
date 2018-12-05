bb65b93e23a8d15e435e98f36d7bf92011508afe
StephenClarkApps <contact.stephenclark@gmail.com>
2018-12-05 13:13:52 +0000

    ...

--- day_five/five.py
+++ day_five/five.py
@@ -1,6 +1,8 @@
 import re
-input_file = open('input.txt').read()
-input = list(input_file)
+raw_input = open('input.txt').read()
+raw_input_list = list(raw_input)
+
+
 
 def opposites(x, y):
     if x.isupper() & y.isupper() | x.islower() & y.islower():
@@ -12,30 +14,52 @@ def opposites(x, y):
 #look at character before target character and after it
 before = []
 after = ['Q']
-def OnePass(input):
+
+# pass in the input as a list and return a list
+def OnePass(list):
+    the_list = list
     counter = 0
-    for x in input:
+    for x in the_list:
         if counter !=0:
-            if input[counter - 1].lower() == input[counter].lower():
+            if the_list[counter - 1].lower() == the_list[counter].lower():
                 #check if have differnt case to each other
-                if opposites(input[counter - 1], input[counter]) == True:
-                    del input[counter]
-                    del input[counter - 1]
+                if opposites(the_list[counter - 1], the_list[counter]) == True:
+                    del the_list[counter]
+                    del the_list[counter - 1]
         counter += 1
+    return the_list
 
     #Check that two Chracters are the same, but have different cases
-def Checks(input):
+def Checks(inputx):
     before = []
     after = ['Q']
     while before != after:
-        before = input.copy()
-        OnePass(input)
-        after = input.copy()
+        before = inputx.copy()
+        after = OnePass(inputx)
+        inputx = after
 
-Checks(input)
+Checks(raw_input_list)
 
 regex = re.compile('[^a-zA-Z]')
-this = ''.join(input)
+this = ''.join(raw_input_list)
 that = regex.sub('', this)
 
 print (len(that))
+
+
+# Part 2
+# cycle through the alphabet removing all a, b, c etc irregardless of cases
+# then do part A on them and find the shortest
+from string import ascii_lowercase
+for ch in ascii_lowercase:
+    print ("The character is: ")
+    print (ch)
+    copy_of_raw_input = raw_input
+    copy_of_raw_input = copy_of_raw_input.replace(ch, "")
+    copy_of_raw_input = copy_of_raw_input.replace(ch.upper(), "")
+    inpu = list(copy_of_raw_input)
+    Checks(inpu)
+    regex = re.compile('[^a-zA-Z]')
+    this = ''.join(inpu)
+    that = regex.sub('', this)
+    print (len(that))
